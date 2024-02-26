591801c6754c85c4fdfd49821f1696d93c3a0f75
hsh30835 <login7h@gmail.com>
2024-02-27 00:49:16 +0900

    main

--- nativetest/apitest/apitest.js
+++ nativetest/apitest/apitest.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Alert, Button, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const FoodApi = () => {
     const [name, setName] = useState('');
@@ -7,7 +7,10 @@ const FoodApi = () => {
     const [clickedNames, setClickedNames] = useState([]);
 
     const findGroupName = () => {
-        fetch(`http://openapi.foodsafetykorea.go.kr/api/0e28c65abe314f1c9981/I2790/json/1/5/DESC_KOR=${name}`) // api에 요청보냄
+
+        const apiUrl = `http://openapi.foodsafetykorea.go.kr/api/0e28c65abe314f1c9981/I2790/json/1/5/DESC_KOR=${name},`
+
+        fetch(apiUrl) // api에 요청보냄
             .then(response => response.json())
             .then(data => {
                 console.log(data)
@@ -15,17 +18,21 @@ const FoodApi = () => {
                     const names = data.I2790.row.map(item => `${"이름 : " + item.DESC_KOR} ${"칼로리 : " + item.NUTR_CONT1} ${"탄수화물 : " + item.NUTR_CONT2} ${"단백질 : " + item.NUTR_CONT3} ${"지방 : " + item.NUTR_CONT4} ${"나트륨 : " + item.NUTR_CONT6}`); // 해당데이터를 출력한다
                     // map : 각 요소를 변환하여 새로운 배열로 매핑하는 역할
                     // 여기서 map을 사용한 이유는 DESC_KOR에 해당하는 값을 꺼내 새로운 배열을 생성했음
+                    console.log(names)
                     setGroupNames(names);
                 } else { // 검색한 data가 존재하지 않을시
-                    const updatedName = name.slice(0, -1); // 맨끝자리를 빼고 재요청
+                    const updatedName = apiUrl.slice(0, -1); // 맨끝자리를 빼고 재요청
+                    console.log(updatedName)
                     console.log("재검색")
-                    fetch(`http://openapi.foodsafetykorea.go.kr/api/0e28c65abe314f1c9981/I2790/json/1/5/DESC_KOR=${updatedName}`)
+                    fetch(updatedName)
                         .then(response => response.json())
                         .then(data => {
                             console.log(data)
                             if (data && data.I2790 && data.I2790.row && data.I2790.row.length > 0) {
                                 const items = data.I2790.row;
                                 const names = items.map(item => `${"이름 : " + item.DESC_KOR} ${"칼로리 : " + item.NUTR_CONT1} ${"탄수화물 : " + item.NUTR_CONT2} ${"단백질 : " + item.NUTR_CONT3} ${"지방 : " + item.NUTR_CONT4} ${"나트륨 : " + item.NUTR_CONT6}`);
+
+                                console.log(names)
                                 setGroupNames(names);
                             }
                         });
@@ -33,8 +40,10 @@ const FoodApi = () => {
             });
     };
 
-    const onChangeHandler = (e) => {
-        setName(e.target.value + ","); // 요청시 뒤에 ,붙임
+    console.log(groupNames)
+
+    const onChangeHandler = (text) => {
+        setName(text); // 요청시 뒤에 ,붙임
     };
 
     const Item = ({ clickedName, removeItem }) => {
@@ -44,7 +53,7 @@ const FoodApi = () => {
 
         return (
             <>
-                <button onClick={removeList}>삭제</button>
+                <Button onPress={removeList} title="삭제"/>
             </>
         )
     }
@@ -67,28 +76,42 @@ const FoodApi = () => {
         <>
             <View>
                 <Text>선택 재료 :</Text>
-                <ul>
-                    {clickedNames.map((name, index) => (
-                        <li key={index}>
-                            {name}
-                            <Item clickedName={name} removeItem={removeItem} />
-                        </li>
-                    ))}
-                </ul>
+                <FlatList
+                    data={clickedNames}
+                    renderItem={({ item }) => (
+                        <View>
+                            <Text>{item}</Text>
+                            <Item clickedName={item} removeItem={removeItem} />
+                        </View>
+                    )}
+                    keyExtractor={(item, index) => index.toString()}
+                />
             </View>
-
+            {/* 육류칸에서 해당하는 동물버튼을 누르면 밑에 부위가 나오고 그 부위를 선택시 선택재료에 들어감 */}
+            <View>
+                <Text>육류</Text>
+                <Button title="소(한우)"/>
+                <Button title="돼지"/>
+                <Button title="닭"/>
+                <Button title="오리"/>
+            </View>
             <View>
                 <Text>식품</Text>
-                <input type="text" onChange={onChangeHandler} />
-                <button onClick={findGroupName}>검색</button>
-                <h3>{name}</h3>
+                <TextInput onChangeText={onChangeHandler} blurOnSubmit={true} placeholder="재료 입력"
+                    keyboardType="default" value={name} />
+                <Button onPress={findGroupName} title="검색" />
+                <Text>{name}</Text>
                 {/* groupNames의 길이가 0보다 클때 실행 */}
                 {groupNames.length > 0 ? (
-                    <ul>
-                        {groupNames.map((groupName, index) => (
-                            <li key={index} onClick={() => ListClickHandler(groupName)}>{groupName}</li>
-                        ))}
-                    </ul>
+                    <FlatList
+                        data={groupNames}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity onPress={() => ListClickHandler(item)}>
+                                <Text>{item}</Text>
+                            </TouchableOpacity>
+                        )}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
                 ) : null}
             </View>
 
